12fd5401aca2323612c90e89954d5cb366e85d93
vThushy <40529679+vThushy@users.noreply.github.com>
2019-02-02 23:19:15 +0530

    Initial Commit

---
+++ JavaScript/fundamentals.js
@@ -0,0 +1,19 @@
+// L1 - Task 1
+// document.write("I am Javascript");
+
+//L1 - Task 2
+// alert("Alert");
+
+
+// let name = "Thushy";
+// var position = "ASE"; 
+
+// const bDay = '11-05-1991';
+
+
+
+var admin, name;
+name = 'John';
+admin = name;
+alert(admin);
+
